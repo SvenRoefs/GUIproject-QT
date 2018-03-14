411a928f986a50579983def06f6338eaf7206edf
SvenRoefs <36294587+SvenRoefs@users.noreply.github.com>
2018-03-14 16:13:40 +0100

    Update 1.1.4
    Working for-loop of QDoubleSpinBoxes

--- generalparameterstab.cpp
+++ generalparameterstab.cpp
@@ -2,19 +2,40 @@
 
 GeneralParametersTab::GeneralParametersTab(QWidget *parent) : QWidget(parent)
 {
-    //ScrollArea Widget
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+    this->setLayout(mainLayout);
+    this->setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::MinimumExpanding);
+
+    // add a ScrollArea widget and define properties
     QScrollArea *scrollArea = new QScrollArea(this);     //Create scroll area Widget
     scrollArea->viewport()->setBackgroundRole(QPalette::Dark); //Give background color
     scrollArea->viewport()->setAutoFillBackground(true); //
+    scrollArea->setContentsMargins(0,0,0,0);
+    scrollArea->setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::MinimumExpanding);
+    scrollArea->setWidgetResizable(true);
 
-    //Create grid for input
+    // add ScrollArea to QWidget GPT
+    mainLayout->addWidget(scrollArea);
+
+    // create new Widget for in the QScrollArea and set properties
+    QWidget* widget = new QWidget();
+    widget->setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::MinimumExpanding);
+
+    // add Widget to ScrollArea
+    scrollArea->setWidget(widget);
+
+    // define GridLayout for the QWidget *inside* the QScrollArea
     QGridLayout *gridLayout = new QGridLayout();
-    QLineEdit *min1 = new QLineEdit("min value of A");
+    widget->setLayout(gridLayout);
 
-    gridLayout->addWidget(min1,0,0,1,1);
+    // add Textboxes to the layout
+    for(int i=0; i<39; i++){
+        this->MinValue.push_back(new QDoubleSpinBox());
+        this->Value.push_back(new QDoubleSpinBox());
+        this->MaxValue.push_back(new QDoubleSpinBox());
 
-    scrollArea->setLayout(gridLayout);
-
-
-
+        gridLayout->addWidget(MinValue[i],i,0);
+        gridLayout->addWidget(Value[i],i,1);
+        gridLayout->addWidget(MaxValue[i],i,2);
+    }
 }
--- generalparameterstab.h
+++ generalparameterstab.h
@@ -5,13 +5,18 @@
 #include <QWidget>
 #include <QScrollArea>
 #include <QGridLayout>
-#include <QLineEdit>
+#include <QDoubleSpinBox>
+#include <QVector>
+#include <iostream>
 
 class GeneralParametersTab : public QWidget
 {
     Q_OBJECT
 private:
     //QGridLayout *gridLayout; //Moet deze hier staan of in mainwindow.h?
+    QVector<QDoubleSpinBox*> MinValue;
+    QVector<QDoubleSpinBox*> Value;
+    QVector<QDoubleSpinBox*> MaxValue;
 public:
     explicit GeneralParametersTab(QWidget *parent = nullptr);
 
