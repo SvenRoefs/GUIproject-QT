d3e3889a5c6531894c2788da5fa1c70e605d0cb3
SvenRoefs <36294587+SvenRoefs@users.noreply.github.com>
2018-04-17 15:23:58 +0200

    Update
    Nu doet hij het wel opeens, ik snap niet waarom nu wel maar ja

--- debug/moc_atomicparameterstab.cpp
+++ debug/moc_atomicparameterstab.cpp
@@ -1,16 +1,16 @@
 /****************************************************************************
-** Meta object code from reading C++ file 'generalparameterstab.h'
+** Meta object code from reading C++ file 'atomicparameterstab.h'
 **
 ** Created by: The Qt Meta Object Compiler version 67 (Qt 5.10.1)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../../GUIproject-QT-master/generalparameterstab.h"
+#include "../atomicparameterstab.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
-#error "The header file 'generalparameterstab.h' doesn't include <QObject>."
+#error "The header file 'atomicparameterstab.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
 #error "This file was generated using the moc from 5.10.1. It"
 #error "cannot be used with the include files from this version of Qt."
@@ -20,25 +20,25 @@
 QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
-struct qt_meta_stringdata_GeneralParametersTab_t {
+struct qt_meta_stringdata_AtomicParametersTab_t {
     QByteArrayData data[1];
-    char stringdata0[21];
+    char stringdata0[20];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
-    qptrdiff(offsetof(qt_meta_stringdata_GeneralParametersTab_t, stringdata0) + ofs \
+    qptrdiff(offsetof(qt_meta_stringdata_AtomicParametersTab_t, stringdata0) + ofs \
         - idx * sizeof(QByteArrayData)) \
     )
-static const qt_meta_stringdata_GeneralParametersTab_t qt_meta_stringdata_GeneralParametersTab = {
+static const qt_meta_stringdata_AtomicParametersTab_t qt_meta_stringdata_AtomicParametersTab = {
     {
-QT_MOC_LITERAL(0, 0, 20) // "GeneralParametersTab"
+QT_MOC_LITERAL(0, 0, 19) // "AtomicParametersTab"
 
     },
-    "GeneralParametersTab"
+    "AtomicParametersTab"
 };
 #undef QT_MOC_LITERAL
 
-static const uint qt_meta_data_GeneralParametersTab[] = {
+static const uint qt_meta_data_AtomicParametersTab[] = {
 
  // content:
        7,       // revision
@@ -54,7 +54,7 @@ static const uint qt_meta_data_GeneralParametersTab[] = {
        0        // eod
 };
 
-void GeneralParametersTab::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+void AtomicParametersTab::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
     Q_UNUSED(_o);
     Q_UNUSED(_id);
@@ -62,26 +62,26 @@ void GeneralParametersTab::qt_static_metacall(QObject *_o, QMetaObject::Call _c,
     Q_UNUSED(_a);
 }
 
-QT_INIT_METAOBJECT const QMetaObject GeneralParametersTab::staticMetaObject = {
-    { &QWidget::staticMetaObject, qt_meta_stringdata_GeneralParametersTab.data,
-      qt_meta_data_GeneralParametersTab,  qt_static_metacall, nullptr, nullptr}
+QT_INIT_METAOBJECT const QMetaObject AtomicParametersTab::staticMetaObject = {
+    { &QWidget::staticMetaObject, qt_meta_stringdata_AtomicParametersTab.data,
+      qt_meta_data_AtomicParametersTab,  qt_static_metacall, nullptr, nullptr}
 };
 
 
-const QMetaObject *GeneralParametersTab::metaObject() const
+const QMetaObject *AtomicParametersTab::metaObject() const
 {
     return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
 }
 
-void *GeneralParametersTab::qt_metacast(const char *_clname)
+void *AtomicParametersTab::qt_metacast(const char *_clname)
 {
     if (!_clname) return nullptr;
-    if (!strcmp(_clname, qt_meta_stringdata_GeneralParametersTab.stringdata0))
+    if (!strcmp(_clname, qt_meta_stringdata_AtomicParametersTab.stringdata0))
         return static_cast<void*>(this);
     return QWidget::qt_metacast(_clname);
 }
 
-int GeneralParametersTab::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+int AtomicParametersTab::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QWidget::qt_metacall(_c, _id, _a);
     return _id;
--- debug/moc_generalparameterstab.cpp
+++ debug/moc_generalparameterstab.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../../GUIproject-QT-master/generalparameterstab.h"
+#include "../generalparameterstab.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
